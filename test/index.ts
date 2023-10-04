f28b54e8d8627e4b1c643f790f49ef000186ceb6
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-04 16:25:00 -0700

    AsyncFilter
    This is hurting me, I wish Array.filter worked with Promises :(
    
    This is some hacky experimentation.
    
    https://stackoverflow.com/questions/7559555/last-modified-file-date-in-node-js
    https://advancedweb.hu/how-to-use-async-functions-with-array-filter-in-javascript/
    https://stackoverflow.com/questions/71600782/async-inside-filter-function-in-javascript
    https://stackoverflow.com/questions/47095019/how-to-use-array-prototype-filter-with-async
    https://stackoverflow.com/questions/64770970/array-filter-with-async-arrow-function
    https://stackoverflow.com/questions/67166932/javascript-array-filter-with-async-await
    https://medium.com/@debbs119/array-filter-and-array-map-with-async-functions-9636e1ae8d6e
    https://michaelheap.com/async-array-filter/ (I want to learn reduce, I still haven't figured it out enough to find myself using it yet)
    https://tech.comtravo.com/javascript/AsyncRay/

--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,47 @@
-import { readFile, writeFile, readdir, mkdir } from "node:fs/promises";
+import { readFile, writeFile, readdir, mkdir, stat } from "node:fs/promises";
 import { join, dirname } from "node:path";
 import { readGamedata, readRegion } from "../src/index.js";
 
-const WORLD = new URL("./ps4",import.meta.url);
+const WORLD = decodeURIComponent(new URL("./ps4",import.meta.url).pathname);
 
-const files = await readdir(WORLD,{ recursive: true, withFileTypes: true })
-  .then(entries => 
-    Promise.all(entries
-      .filter(entry => entry.isFile())
-      .map(async ({ name, path }) => {
-        const pathname = join(path,name);
-        const buffer = await readFile(pathname);
-        return new File([buffer],pathname.split("ps4/").pop()!);
-      })
-  ));
+const oldFilter = Array.prototype.filter;
+
+declare global {
+  interface Array<T> {
+    asyncFilter(...args: Parameters<typeof Array.prototype.filter>): Promise<T[]>;
+  }
+}
+
+Array.prototype.asyncFilter = async function(...args: Parameters<typeof Array.prototype.filter>){
+  const result = oldFilter.call(this,...args);
+  await Promise.all(result);
+  return result;
+};
+
+async function walk(dir: string) {
+  const entries = (await readdir(dir,{ recursive: true })).asyncFilter(async entry => {
+    const path = join(dir,entry);
+    const stats = await stat(path);
+    console.log(stats.isDirectory());
+    return stats.isFile();
+  });
+  return entries;
+}
+
+const files = await walk(WORLD);
 console.log(files);
+
+// const files = await readdir(WORLD,{ recursive: true })
+//   .then(entries => 
+//     Promise.all(entries
+//       .filter(entry => entry.isFile())
+//       .map(async ({ name, path }) => {
+//         const pathname = join(path,name);
+//         const buffer = await readFile(pathname);
+//         return new File([buffer],pathname.split("ps4/").pop()!);
+//       })
+//   ));
+// console.log(files);
 
 // const GAMEDATA = new URL("./ps4/GAMEDATA",import.meta.url);
 

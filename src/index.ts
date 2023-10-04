f6b9be6f12001bc2cd3b486ec6f80dda39f06a1f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-04 01:13:24 -0700

    'Region Files' Demo
    Trying to deduce what the heck these additional `GAMEDATA_*` files are! I'm pretty sure they are the missing `.mcr` files that are usually present in the standalone `GAMEDATA` file, but I'm not sure how you are supposed to distinguish them apart. Do they hold their file name anywhere?
    
    My initial guess is that the start of the file stores the `r.*.*.mcr` numbers, but I'm not totally sure. I'm just guessing here, trying things out. The current version is just because the values it return appear to kind of look like what we might want to find.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./read.js";
 export * from "./header.js";
 export * from "./definition.js";
+export * from "./region.js";
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,15 @@
 import { readFile, writeFile, readdir, mkdir } from "node:fs/promises";
 import { join, dirname } from "node:path";
-import { read } from "../src/index.js";
+import { read, readRegion } from "../src/index.js";
 
-const GAMEDATA = new URL("./ps4/GAMEDATA",import.meta.url);
-const REGIONS = await (
-  readdir(new URL("./ps4",import.meta.url),{ withFileTypes: true })
-    .then(entries => entries
-      .filter(entry => entry.isFile() && /^GAMEDATA_/.test(entry.name))
-      .map(entry => new URL(entry.name,import.meta.url))
-    )
-)
-console.log(REGIONS);
+// const GAMEDATA = new URL("./ps4/GAMEDATA",import.meta.url);
 
-const data = await readFile(GAMEDATA);
-const files = read(data,"ps4");
+const REGIONS = (await readdir(new URL("./ps4",import.meta.url),{ withFileTypes: true }))
+  .filter(entry => entry.isFile() && /^GAMEDATA_/.test(entry.name))
+  .map(entry => new URL(join("./ps4",entry.name),import.meta.url));
+
+// const data = await readFile(GAMEDATA);
+// const files = read(data,"ps4");
 
 // for (const file of files){
 //   console.log(file);
@@ -22,3 +18,10 @@ const files = read(data,"ps4");
 //   await mkdir(dirname(path),{ recursive: true });
 //   writeFile(path,new Uint8Array(await file.arrayBuffer()));
 // }
+
+const regionData = await Promise.all(REGIONS.map(entry => readFile(entry)));
+const regionFiles = regionData.map(readRegion);
+
+for (const region of regionFiles){
+  // console.log(region);
+}

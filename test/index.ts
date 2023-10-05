4ce18225d5f5d91f6b3c2b0358732fb1c3f02850
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-04 18:01:38 -0700

    File-System-Access (Node & browser?)
    I know how to use the File System Access API, as well as Node's FS module, the hard thing is implementing both so this project can work in the browser directly, or on the terminal/locally. Then I thought it might make sense to use the FSA API structures across the board to manage things, no matter where the program is running, but I'm not sure this is fully the best way to go. I'm going to look into other options.
    
    My first idea that I started working on, but started to feel messy, was working with `File` objects directly. That seems hard to manage though, since you have to build and tear them apart yourself. It's the next best thing to working right in the browser though, so I'm trying to figure out which way to go here. I may end up making my own polyfill/library sort of thing to manage this. It could be way easier, I just haven't found what I'm looking for yet.
    
    https://developer.mozilla.org/en-US/docs/Web/API/File_System_API
    https://developer.mozilla.org/en-US/docs/Web/API/StorageManager/getDirectory
    https://developer.mozilla.org/en-US/docs/Web/API/Navigator/storage
    https://developer.chrome.com/articles/file-system-access/#creating-or-accessing-files-and-folders-in-a-directory
    https://developer.chrome.com/articles/file-system-access/#accessing-the-origin-private-file-system (Inspiration for bringing this implementation to both Node and the browser, by making the APIs the same to use under the hood)
    https://www.npmjs.com/package/file-system-access
    https://github.com/use-strict/file-system-access

--- test/index.ts
+++ test/index.ts
@@ -1,55 +1,12 @@
-import { readFile, writeFile, readdir, mkdir, stat } from "node:fs/promises";
-import { join, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { getOriginPrivateDirectory, support } from "file-system-access";
 import { readGamedata, readRegion } from "../src/index.js";
 
 const WORLD = new URL("./ps4",import.meta.url);
 
-async function walk(dir: string | URL): Promise<File[]> {
-  const root = fileURLToPath(dir);
-  const entries = await readdir(dir,{ recursive: true });
-  const files = await Promise.all((await Promise.all(entries
-      .map(async entry => [await stat(join(root,entry)),entry] as const)))
-    .filter(([stats]) => stats.isFile())
-    .map(async ([stats,entry]) => new File([await readFile(join(root,entry))],entry,{ lastModified: stats.mtimeMs })));
-  return files;
+console.log(support);
+
+const world = await getOriginPrivateDirectory(import("file-system-access/lib/adapters/node.js"),decodeURIComponent(WORLD.pathname));
+
+for await (const [,file] of world){
+  console.log(file);
 }
-
-const files = await walk(WORLD);
-console.log(files);
-
-// const files = await readdir(WORLD,{ recursive: true })
-//   .then(entries => 
-//     Promise.all(entries
-//       .filter(entry => entry.isFile())
-//       .map(async ({ name, path }) => {
-//         const pathname = join(path,name);
-//         const buffer = await readFile(pathname);
-//         return new File([buffer],pathname.split("ps4/").pop()!);
-//       })
-//   ));
-// console.log(files);
-
-// const GAMEDATA = new URL("./ps4/GAMEDATA",import.meta.url);
-
-// const REGIONS = (await readdir(new URL("./ps4",import.meta.url),{ withFileTypes: true }))
-//   .filter(entry => entry.isFile() && /^GAMEDATA_/.test(entry.name))
-//   .map(entry => new URL(join("./ps4",entry.name),import.meta.url));
-
-// const data = await readFile(GAMEDATA);
-// const files = readGamedata(data,"ps4");
-
-// for (const file of files){
-//   console.log(file);
-//   const path = decodeURIComponent(new URL(join("./world_data",file.name),import.meta.url).pathname);
-
-//   await mkdir(dirname(path),{ recursive: true });
-//   writeFile(path,new Uint8Array(await file.arrayBuffer()));
-// }
-
-// const regionData = await Promise.all(REGIONS.map(entry => readFile(entry)));
-// const regionFiles = regionData.map(readRegion);
-
-// for (const region of regionFiles){
-//   // console.log(region);
-// }

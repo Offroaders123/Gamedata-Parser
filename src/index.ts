5f1d6883411eb689c20ae4f70e79f54bd97de926
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-12 15:02:21 -0700

    Parsing Test Reset
    Brought the test script/main source back to it's regular state before my file system abstraction ideas.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from "./gamedata.js";
 export * from "./header.js";
 export * from "./definition.js";
-export * from "./region.js";
--- src/region.ts
+++
@@ -1,6 +0,0 @@
-export function readRegion(data: Uint8Array): Uint8Array {
-  console.log(data.subarray(0,16));
-  const view = new DataView(data.buffer,data.byteOffset,16);
-  console.log(view.getUint16(0,false),view.getUint16(2,false));
-  return data;
-}

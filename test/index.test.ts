a8ec747122e0fab2b0f4b0a98f07ba8adc051f43
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-04-08 21:41:15 -0700

    PS Vita World Test
    Curious about what compression the top-level of a PS Vita save file uses.
    
    I tried zlib, gzip, raw deflate, and RLE, and none of them work.
    
    The file itself doesn't appear to have any of the regular appearances for any of those either, but I thought I might as well try by way of looping through the whole file to see if it could find any compressed parts in those formats.
    
    Not sure whether the docs for this are accurate, but it doesn't seem to be lining up unfortunately.
    https://github.com/PhoenixARC/Minecraft-LCE-Docs/blob/main/Documentation.md#lce-buffers

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,6 +8,7 @@ import type { Platform } from "../src/index.js";
 const paths: [Platform, string][] = [
   ["xbox-360", "./xbox-360/savegame-decompressed.dat"],
   ["ps3", "./ps3/GAMEDATA"],
+  ["ps-vita", "./ps-vita/GAMEDATA.bin"],
   ["wii-u", "./wii-u/savegame.wii"],
   // ["ps4", "./ps4/GAMEDATA"]
 ];

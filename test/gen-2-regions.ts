8bb65eb0a88ae3e7749a1ea17e71db87c6aa984d
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-04-14 02:49:59 -0700

    Unzip Demolition
    Trying to break apart the region files to see if they are compressed in one way or another, but looks like it isn't quite exactly.
    
    https://sometimeinfebruary.bandcamp.com/album/there-goes

--- test/gen-2-regions.ts
+++ test/gen-2-regions.ts
@@ -1,7 +1,9 @@
 import { join } from "node:path";
 import { type Region, readRegion } from "../../MCRegionJS/src/index.js";
-import { readFile, readdir } from "node:fs/promises";
+import { readFile, readdir, writeFile } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
+import { inflate } from "node:zlib";
+import { promisify } from "node:util";
 
 const ps4Path: string = fileURLToPath(new URL("./ps4", import.meta.url));
 
@@ -14,14 +16,32 @@ console.log(filePaths);
 // const coordinates: number[][] = coordinateStrings.map(string => [...new Int8Array(Buffer.from(string, "hex"))]);
 // console.log(coordinates);
 
-const files: Buffer[] = await Promise.all(filePaths.map(path => readFile(join(ps4Path, path))));
+const files: [string, Buffer][] = await Promise.all(filePaths.map(async path => [join(ps4Path, path), await readFile(join(ps4Path, path))]));
 console.log(files);
 
-const regions: (Region | Error)[] = files.map(file => {
-  try {
-    return readRegion(file);
-  } catch (error){
-    return error as Error;
+// const regions: (Region | Error)[] = files.map(file => {
+//   try {
+//     return readRegion(file);
+//   } catch (error){
+//     return error as Error;
+//   }
+// });
+// console.log(regions);
+
+const regions2: [string, Buffer | null][] = await Promise.all(files.map(async ([name, file]) => {
+  let i = 0;
+  while (i < file.byteLength){
+    try {
+      return [name, await promisify(inflate)(file.subarray(i))];
+    } catch {
+      i++;
+    }
   }
-});
-console.log(regions);
+  console.log(name, i);
+  return [name, null];
+}));
+console.log(regions2);
+
+for (const [name, file] of regions2){
+  await writeFile(`${name}.two`, file ?? []);
+}

28d5ebbcc9ca1dbd06ed3067d4569dcc7faf453f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-04-14 02:25:21 -0700

    PS4 Region Testing + DEV Ai (Devy, not Devin AI)
    Curious about trying to open PS4 region files with my MCRegionJS project. It seemed to ultimately fail, not sure yet quite what format it uses. It seems to have some zlib headers throughout the file though, so it does seem to have fairly normal data in there.
    
    https://www.youtube.com/watch?v=csqWz2SwEWg&list=PLpA5ThqzxTYWSVed_q3aWfMZ1jx8Adj9m&index=1

--- test/gen-2-regions.ts
+++ test/gen-2-regions.ts
@@ -1,12 +1,27 @@
-import { readdir } from "node:fs/promises";
+import { join } from "node:path";
+import { type Region, readRegion } from "../../MCRegionJS/src/index.js";
+import { readFile, readdir } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 
-const ps4Path = new URL("./ps4", import.meta.url);
+const ps4Path: string = fileURLToPath(new URL("./ps4", import.meta.url));
 
-const files: string[] = (await readdir(ps4Path)).filter(name => name.startsWith("GAMEDATA_"));
+const filePaths: string[] = (await readdir(ps4Path)).filter(name => name.startsWith("GAMEDATA_"));
+console.log(filePaths);
+
+// const coordinateStrings: string[] = filePaths.map(file => file.replace(/^GAMEDATA_/, ""));
+// console.log(coordinateStrings);
+
+// const coordinates: number[][] = coordinateStrings.map(string => [...new Int8Array(Buffer.from(string, "hex"))]);
+// console.log(coordinates);
+
+const files: Buffer[] = await Promise.all(filePaths.map(path => readFile(join(ps4Path, path))));
 console.log(files);
 
-const coordinateStrings: string[] = files.map(file => file.replace(/^GAMEDATA_/, ""));
-console.log(coordinateStrings);
-
-const coordinates: number[][] = coordinateStrings.map(string => [...new Int8Array(Buffer.from(string, "hex"))]);
-console.log(coordinates);
+const regions: (Region | Error)[] = files.map(file => {
+  try {
+    return readRegion(file);
+  } catch (error){
+    return error as Error;
+  }
+});
+console.log(regions);
